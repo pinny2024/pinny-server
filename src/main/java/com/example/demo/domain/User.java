74baf697a7f7e614f9bc47c938cbbcac4502bc66
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-03-11 23:26:39 +0900

    feat : test

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Builder
+@Table
+public class User {
+}
