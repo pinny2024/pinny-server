ad37f44c18cfbb75201ae961d7cc32c0fc6f577c
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-03-19 19:28:29 +0900

    error : 회원가입 api

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,53 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
+    private Long userId;
+
+    @Column(name = "email", length = 20)
+    private String email;
+
+    @Column(name = "password", length = 20)
+    private String password;
+
+    @Column(name = "nickname", length = 20)
+    private String nickname;
+
+    @Column(name = "career", length = 20)
+    private String career;
+
+    @Column(name = "profile", length = 255)
+    private String profile;
+
+    @Column(name = "salary")
+    private Integer salary;
+
+    @Column(name = "saving")
+    private Integer saving;
+
+    @Column(name = "ageRange")
+    private Integer ageRange;
+
+    @Column(name = "introduction", length = 255)
+    private String introduction;
+
+    @ElementCollection
+    @CollectionTable(name = "user_badges", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "badge")
+    private List<String> badges;
+
+}
