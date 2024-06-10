15ba744d6f5a9d9e5f04d712054ff980b014b7fb
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-06-11 08:37:46 +0900

    reafactor : 카테고리 api 코드 수정

--- src/main/java/com/example/demo/repository/ExpenditureRepository.java
+++ src/main/java/com/example/demo/repository/ExpenditureRepository.java
@@ -2,9 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Expenditure;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> {
     List<Expenditure> findByCategory(String category);
     List<Expenditure> findByUserId(Long userId);
+    List<Expenditure> findByUserIdAndCategory(Long userId, String category);
 }
