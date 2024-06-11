23ec0e25e403310d31a47e8d572c28e092702d8f
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-06-12 00:03:32 +0900

    feat : 따로보기 api 완성

--- src/main/java/com/example/demo/service/TransactionService.java
+++ src/main/java/com/example/demo/service/TransactionService.java
@@ -1,11 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.Category;
 import com.example.demo.domain.Transaction;
 import com.example.demo.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class TransactionService {
@@ -23,5 +25,13 @@ public class TransactionService {
 
     public List<Transaction> getAllTransactions() {
         return transactionRepository.findAll();
+    }
+
+    public List<Transaction> getTransactionsByCategory(Category category) {
+        List<Transaction> transactions = transactionRepository.findByCategory(category);
+        if (transactions.isEmpty()) {
+            throw new NoSuchElementException("해당 카테고리에 대한 정보가 없습니다.");
+        }
+        return transactions;
     }
 }
