e255beb66a334fb8a5050bd100cace81d602bce5
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-06-12 00:41:34 +0900

    refactor : 로컬타임 추가

--- src/main/java/com/example/demo/domain/Category.java
+++ src/main/java/com/example/demo/domain/Category.java
@@ -1,11 +1,19 @@
 package com.example.demo.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Category {
     식비,            // 식비
     교통수단,  // 교통수단
     문화생활,         // 문화생활
     금융,         // 금융
     기타,            // 기타
-    저축
+    저축;
 
+    // 모든 카테고리 목록을 가져옴
+    public static List<String> getAllCategories() {
+        return Arrays.asList(values()).stream().map(Enum::name).collect(Collectors.toList());
+    }
 }
--- src/main/java/com/example/demo/domain/Transaction.java
+++ src/main/java/com/example/demo/domain/Transaction.java
@@ -1,10 +1,20 @@
 package com.example.demo.domain;
 
+import com.example.demo.domain.Category;
+import com.example.demo.domain.Type;
 import jakarta.persistence.*;
-import java.math.BigDecimal;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "transactions") // 테이블 이름 지정
+@EntityListeners(AuditingEntityListener.class)
 public class Transaction {
 
     @Id
@@ -25,56 +35,6 @@ public class Transaction {
     @Column(nullable = false)
     private String description;
 
-    // 기본 생성자
-    public Transaction() {
-    }
-
-    // 모든 필드를 포함한 생성자
-    public Transaction(BigDecimal amount, Category category, Type type, String description) {
-        this.amount = amount;
-        this.category = category;
-        this.type = type;
-        this.description = description;
-    }
-
-    // Getter 및 Setter 메서드
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
