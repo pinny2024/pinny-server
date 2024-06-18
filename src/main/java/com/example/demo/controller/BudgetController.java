cc211ca5818401d92902123262039a85feb42acf
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-06-18 10:47:56 +0900

    refactor : response 수정 및 예산 수정 api 완성

--- src/main/java/com/example/demo/controller/BudgetController.java
+++ src/main/java/com/example/demo/controller/BudgetController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.Budget;
+import com.example.demo.dto.BudgetDTO;
 import com.example.demo.service.BudgetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,14 +20,27 @@ public class BudgetController {
     }
 
     @PostMapping
-    public ResponseEntity<Budget> createBudget(@PathVariable("userId") Long userId, @RequestParam Integer totalBudget) {
-        Budget createdBudget = budgetService.createBudget(userId, totalBudget);
-        return new ResponseEntity<>(createdBudget, HttpStatus.CREATED);
+    public ResponseEntity<String> createBudget(@PathVariable("userId") Long userId, @RequestBody BudgetDTO budgetDTO) {
+        Budget createdBudget = budgetService.createBudget(userId, budgetDTO.getTotalBudget());
+        String message = String.format("%d원으로 설정하셨습니다.", createdBudget.getTotalBudget());
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateBudget(@PathVariable("userId") Long userId, @RequestBody BudgetDTO budgetDTO) {
+        try {
+            Budget updatedBudget = budgetService.updateTotalBudget(userId, budgetDTO.getTotalBudget());
+            String message = String.format("%d원으로 예산을 수정하셨습니다.", updatedBudget.getTotalBudget());
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping
-    public ResponseEntity<Budget> getBudget(@PathVariable("userId") Long userId) {
+    public ResponseEntity<String> getBudget(@PathVariable("userId") Long userId) {
         Budget budget = budgetService.getBudgetByUserId(userId);
-        return new ResponseEntity<>(budget, HttpStatus.OK);
+        String message = String.format("남은금액 %d원 남았습니다.", budget.getRemainingBudget());
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
